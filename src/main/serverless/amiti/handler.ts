7dfba122146c2cc472f49a62d7075e8691f36084
umesh-amiti <umesh@amitisoft.com>
2017-05-31 10:55:06 +0530

    QuestionPaper mgmt
    QuestionPaper mgmt

--- src/main/serverless/amiti/handler.ts
+++ src/main/serverless/amiti/handler.ts
@@ -52,10 +52,9 @@ let bookingServiceImplFactory = (candidateServiceImpl: CandidateServiceImpl) => 
     return new BookingServiceImpl(process.env.ELASTICSEARCH_ENDPOINT, process.env.REGION, new DocumentClient(), candidateServiceImpl);
 };
 
-let questionServiceImplFactory = (qsnIdsServiceImpl: QsnIdsServiceImpl,bookingServiceImpl:BookingServiceImpl) => {
+let questionServiceImplFactory = (qsnIdsServiceImpl: QsnIdsServiceImpl,bookingServiceImpl: BookingServiceImpl) => {
       return new QuestionServiceImpl(process.env.REGION, new DocumentClient(), qsnIdsServiceImpl,bookingServiceImpl);
-      
-}
+};
 
 let resultServiceImplFactory = (candidateServiceImpl: CandidateServiceImpl , bookingServiceImpl: BookingServiceImpl, questionServiceImpl: QuestionServiceImpl) => {
     console.log(`in process bookingServiceImplFactory ${JSON.stringify(process.env.ELASTICSEARCH_ENDPOINT)}`);
@@ -128,8 +127,9 @@ exports.findESResultSearch = ExecutionContextImpl.createHttpHandler(appProviders
 exports.getTestStausInfo = ExecutionContextImpl.createHttpHandler(appProviders, GetBookingHandler.isTestLinkStatusInfo);
 exports.updateExamBookingTimings = ExecutionContextImpl.createHttpHandler(appProviders, GetBookingHandler.updateExamTimingSlots);
 exports.findESCandidateSearchResult = ExecutionContextImpl.createHttpHandler(appProviders, GetCandidateHandler.findESCandidateSearchResult);
- exports.createCategory = ExecutionContextImpl.createHttpHandler(appProviders, CategoryHandler.createCategory);
- exports.getAllCategories = ExecutionContextImpl.createHttpHandler(appProviders, CategoryHandler.getAllCategories);
- exports.getCategoryById = ExecutionContextImpl.createHttpHandler(appProviders, CategoryHandler.getCategoryById);
+exports.createCategory = ExecutionContextImpl.createHttpHandler(appProviders, CategoryHandler.createCategory);
+exports.getAllCategories = ExecutionContextImpl.createHttpHandler(appProviders, CategoryHandler.getAllCategories);
+exports.getCategoryById = ExecutionContextImpl.createHttpHandler(appProviders, CategoryHandler.getCategoryById);
+exports.getAllQuestionsByPaperId = ExecutionContextImpl.createHttpHandler(appProviders, CreateQuestionHandler.getAllQuestionsByPaperId);
 
 
--- src/main/serverless/amiti/typescript/client/web/create-question-handler.ts
+++ src/main/serverless/amiti/typescript/client/web/create-question-handler.ts
@@ -1,13 +1,11 @@
 import { CreateQuestionFacade } from '../facade/create-question-facade';
 import { Injector } from '@angular/core';
-import { HttpContextImpl } from "../../api/http/http-context-impl";
+import { HttpContextImpl } from '../../api/http/http-context-impl';
 
 export class CreateQuestionHandler {
 
-    static createQuestion (httpContext:HttpContextImpl,injector:Injector) : void {
-          console.log("CreateQuestionHandler");
+    static createQuestion (httpContext: HttpContextImpl,injector: Injector): void {
         let body = httpContext.getRequestBody();
-        console.log("pathParameters-----",body);
         injector.get(CreateQuestionFacade).createQuestion(body)
             .subscribe(result => {
                 httpContext.ok(200, result);
@@ -16,15 +14,11 @@ export class CreateQuestionHandler {
         });
     }
 
-static getQuestionByCategory(httpContext:HttpContextImpl,injector:Injector):void{
+static getQuestionByCategory(httpContext: HttpContextImpl,injector: Injector): void {
 
-    let pathParam = httpContext.getPathParameters();
-
-        console.log("pathParameters-----??????????",pathParam);
-
-        let categoryId = pathParam["Category"];
-        let lastqsnid = pathParam["LastqsnId"];
-       console.log("lastqsnid......",lastqsnid);
+        let pathParam = httpContext.getPathParameters();
+        let categoryId = pathParam['Category'];
+        let lastqsnid = pathParam['LastqsnId'];
         injector.get(CreateQuestionFacade).findbyCategory(categoryId,lastqsnid)
             .subscribe(result => {
                 httpContext.ok(200, result);
@@ -33,4 +27,16 @@ static getQuestionByCategory(httpContext:HttpContextImpl,injector:Injector):void
             });
 }
 
+
+static getAllQuestionsByPaperId (httpContext: HttpContextImpl,injector: Injector): void {
+
+       let pathParam = httpContext.getPathParameters();
+       let qsnPaperId = pathParam['qsnPaperId'];
+       injector.get(CreateQuestionFacade).getAllQuestionsByPaperId(qsnPaperId)
+            .subscribe(result => {
+                httpContext.ok(200, result);
+            },  err => {
+                httpContext.fail(err, 500);
+            });
+  }
 }
